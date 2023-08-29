1b5e58423d096b1295b6432936a9ad41ed31b3a1
astarta16 <ngedeshuri2001@gmail.com>
2023-08-29 21:12:17 +0400

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import Home from "./pages/Home";
-import Crew from "./pages/Crew";
-import Destination from "./pages/Destination";
-import Tecnology from "./pages/Tecnology";
+// import Home from "./pages/Home";
+// import Crew from "./pages/Crew";
+// import Destination from "./pages/Destination";
+// import Tecnology from "./pages/Tecnology";
 import "./app.css";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from "./components/Header/header";
 
 
-function App() {
+const App = () => {
   return (
-    <>
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
-        <Route path="/crew" element={<Crew />} />
-        <Route path="/tecnology" element={<Tecnology />} />
-      </Routes>
+      <Header/>
+      <main>
+        <Routes />
+      </main>
     </Router>
-    </>
   );
-}
-
+};
 export default App;
